be0227b92bd9e72d4c4f42a8987c64236fff179c
Diego Hidalgo <d.hidalgo.pe@gmail.com>
2025-01-28 18:32:03 -0800

    First version for the scanning.py

--- scanning.py
+++ scanning.py
@@ -1,3 +1,19 @@
 
-def test():
-    
+print("Input text: ")
+text1 = input()
+print("Input text to find: ")
+textToFind1 = input()
+
+test(text1, textToFind1)
+
+
+def test(text, textToFind):
+    length = len(textToFind)
+    current = 0
+    for char in text:
+        if char == textToFind[current]:
+            current += 1
+            if current == length:
+                return True
+            
+    return False
